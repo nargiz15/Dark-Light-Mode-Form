5a457add09dd8429ea7583e191d1a99ab3f613aa
Nargiz Azizli <143933432+nargiz15@users.noreply.github.com>
2024-02-01 01:09:35 -0800

    Dark/Light theme Form

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const darkmood = document.querySelector(".iconmoon")
+const darkmood = document.querySelector(".checkbox")
 const title = document.querySelector(".title")
 
 darkmood.addEventListener("click", function(){
       // document.body.classList.toggle("dark")
             if( document.body.classList.toggle("dark")){
               title.textContent=("Dark Theme")
+              
             }
             else(
                   title.textContent=("Light Theme") 
